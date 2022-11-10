0233e724f85a076bb288d5e9e1a528845207ea1e
James <bumboy@gmail.com>
2022-11-10 16:28:49 -0600

    in place text area with previous text

--- src/App.js
+++ src/App.js
@@ -18,12 +18,14 @@ function App() {
   }
   return (
     <div className="App container">
-        <div>
+        <div className="d-flex flex-row justify-content-between">
           <h1>scrblswyp</h1>
-          <input placeholder="Username" onChange={e => setLoginUsername(e.target.value)} />
-          <input placeholder="Password" onChange={e => setLoginPassword(e.target.value)} />
-          <button className="btn btn-success" onClick={loginAccount}>Login</button>
-          <button className="btn btn-danger" onClick={() => setShowModal(true)}>Create Account</button>
+          <div className="mt-2">
+            <input placeholder="Username" className="mr-2" onChange={e => setLoginUsername(e.target.value)} />
+            <input placeholder="Password" onChange={e => setLoginPassword(e.target.value)} />
+            <button className="btn btn-success"  onClick={loginAccount}>Login</button>
+            <button className="btn btn-danger" onClick={() => setShowModal(true)}>Create Account</button>
+          </div>
           {showModal && <RegisterModal setShowModal={setShowModal} />}
 
         </div>
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Button } from './Button';
+import '../App.css';
 
 export const Note = () => {
+    const [text, setText] = React.useState("");
+    const [editOn, setEditOn] = React.useState(false)
 
     const [active, setActive] = React.useState(false);
 
@@ -9,11 +12,28 @@ export const Note = () => {
     const handleClick = () => {
         setActive(!active)
     }
+
+    const editText = (e) => {
+      console.log(e.target.previousElementSibling.innerText)
+      setText(e.target.previousElementSibling.innerText)
+      setEditOn(!editOn)
+    }
+
+    const cancelEdit = () => {
+      setActive(false)
+      setEditOn(false)
+    }
   return (
     <div className="container note-normal">
         <h2 className="mb-4">This is what a note would look like</h2>
-        <p onClick={handleClick}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        {active && <Button className="btn btn-info" label="Edit"></Button>}
+        {!editOn && <p onClick={handleClick}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>}
+        {editOn && <div>
+          <textarea className="form-control" value={text}></textarea>
+          <button className="btn btn-danger">Delete Item</button>
+          <button className="btn btn-success">Save</button>
+          <button className="btn btn-warning" onClick={cancelEdit}>Cancel</button>
+          </div>}
+        {active && !editOn && <button className="btn btn-info" onClick={editText} label="Edit">Edit</button>}
     </div>
   )
 }
